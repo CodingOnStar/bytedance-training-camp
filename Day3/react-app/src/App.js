aba30fa2a8fc4d6d1fca09077c0058d72ce1e729
tencent_hanxujiang <hanxujiang@tencent.com>
2021-09-22 11:50:13 +0800

    feat:antd

---
+++ Day3/react-app/src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import { Input, Menu, Row, Col } from "antd";
+import { UnorderedListOutlined } from "@ant-design/icons";
+
+function App() {
+  return (
+    <div className="App">
+      <Row>
+        <Col>
+          <a id="logo" href="/">
+            <img
+              alt="logo"
+              src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+            ></img>
+            Ant Design
+          </a>
+        </Col>
+        <Col flex="1">
+          <div id="searchbox">
+            <Input placeholder="搜索"></Input>
+          </div>
+        </Col>
+        <Col>
+          <Menu mode="horizontal" id="menu">
+            <Menu.Item>设计</Menu.Item>
+            <Menu.Item>文档</Menu.Item>
+            <Menu.Item>组件</Menu.Item>
+            <Menu.Item>资源</Menu.Item>
+            <Menu.Item>国内镜像</Menu.Item>
+            <Menu.SubMenu title="子菜单" icon={<UnorderedListOutlined />}>
+              <Menu.Item>子菜单项</Menu.Item>
+            </Menu.SubMenu>
+          </Menu>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default App;
